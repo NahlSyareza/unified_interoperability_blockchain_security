df56050f4c9fcc5cfe65360636f56e3e9f6dae2e
NahlSyareza <availablearner@protonmail.com>
2026-02-10 01:20:15 +0700

    Implemented multiple BLE connection

---
+++ include/ble_handler.hpp
@@ -0,0 +1,8 @@
+#include <iomanip>
+#include <iostream>
+#include <simpleble/SimpleBLE.h>
+#include <spdlog/spdlog.h>
+#include <utils.hpp>
+#include <vector>
+
+int ble_handler() { return EXIT_FAILURE; }
--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -28,36 +28,44 @@ public:
     (*mp)[key]->payload = payload;
   }
 
-  std::map<string, json> connection_registers_m;
-  std::map<string, json> device_profiles_m;
-  std::map<string, json> device_registers_m;
-  std::map<string, json> format_profiles_m;
+  std::map<string, json> connection_registers;
+  std::map<string, json> device_profiles;
+  std::map<string, json> device_registers;
+  std::map<string, json> format_profiles;
+
+  std::map<string, json> ble_addresses;
 
   void fill_maps() {
     ifstream connection_registers_f("./include/config/connection_registers.json");
     ifstream device_profiles_f("./include/config/device_profiles.json");
     ifstream device_registers_f("./include/config/device_registers.json");
     ifstream format_profiles_f("./include/config/format_profiles.json");
+    ifstream ble_addresses_f("./include/config/ble_addresses.json");
 
-    json connection_registers_j = json::parse(connection_registers_f);
-    json device_profiles_j = json::parse(device_profiles_f);
-    json device_registers_j = json::parse(device_registers_f);
-    json format_profiles_j = json::parse(format_profiles_f);
+    json parse_connection_registers = json::parse(connection_registers_f);
+    json parse_device_profiles = json::parse(device_profiles_f);
+    json parse_device_registers = json::parse(device_registers_f);
+    json parse_format_profiles = json::parse(format_profiles_f);
+    json parse_ble_addresses = json::parse(ble_addresses_f);
 
-    for (const auto &item : connection_registers_j) {
-      connection_registers_m[item["source"]] = item;
+    for (const auto &item : parse_connection_registers) {
+      connection_registers[item["source"]] = item;
     }
 
-    for (const auto &item : device_profiles_j) {
-      device_profiles_m[item["name"]] = item;
+    for (const auto &item : parse_device_profiles) {
+      device_profiles[item["name"]] = item;
     }
 
-    for (const auto &item : device_registers_j) {
-      device_registers_m[item["device"]] = item;
+    for (const auto &item : parse_device_registers) {
+      device_registers[item["device"]] = item;
     }
 
-    for (const auto &item : format_profiles_j) {
-      format_profiles_m[item["name"]] = item;
+    for (const auto &item : parse_format_profiles) {
+      format_profiles[item["name"]] = item;
+    }
+
+    for (const auto &item : parse_ble_addresses) {
+      ble_addresses[item["identifier"]] = item;
     }
   }
 };
