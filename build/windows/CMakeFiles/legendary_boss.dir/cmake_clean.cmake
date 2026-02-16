bd01d43bdec34dbaeadcf8875900942e3ca50859
NahlSyareza <availablearner@protonmail.com>
2026-02-17 01:08:33 +0700

    Build for windows

---
+++ build/windows/CMakeFiles/legendary_boss.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/legendary_boss.dir/main.cpp.obj"
+  "CMakeFiles/legendary_boss.dir/main.cpp.obj.d"
+  "CMakeFiles/legendary_boss.dir/utils.cpp.obj"
+  "CMakeFiles/legendary_boss.dir/utils.cpp.obj.d"
+  "legendary_boss.exe"
+  "legendary_boss.exe.manifest"
+  "legendary_boss.lib"
+  "legendary_boss.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/legendary_boss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
