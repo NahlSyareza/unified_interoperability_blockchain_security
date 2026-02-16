084c06c4818d3562042420fb1525619965b6a50d
NahlSyareza <rahidrasyarezanahl@gmail.com>
2026-02-17 01:14:45 +0700

    Build for Linux

---
+++ build/linux/CMakeFiles/legendary_boss.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/legendary_boss.dir/main.cpp.o"
+  "CMakeFiles/legendary_boss.dir/main.cpp.o.d"
+  "CMakeFiles/legendary_boss.dir/utils.cpp.o"
+  "CMakeFiles/legendary_boss.dir/utils.cpp.o.d"
+  "legendary_boss"
+  "legendary_boss.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/legendary_boss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/linux/CMakeFiles/legendary_boss.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for legendary_boss.
+# This may be replaced when dependencies are built.
