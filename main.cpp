4ea0dd81f81ef1874f2c7906d1ad645c35aee19e
Nahl.Syareza <rahidrasyarezanahl@gmail.com>
2026-04-23 00:10:53 +0700

    Trying to resolve BLE issues

--- main.cpp
+++ main.cpp
@@ -25,11 +25,11 @@ int main() {
 
   std::thread mqtt_thread(mqtt_handler, &ds);
   std::thread http_thread(http_handler, &ds);
-//  std::thread ble_thread(ble_handler, &ds);
+  std::thread ble_thread(ble_handler, &ds);
 
   mqtt_thread.join();
   http_thread.join();
-//  ble_thread.join();
+  ble_thread.join();
 
   return 0;
 }
